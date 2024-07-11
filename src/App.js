8c4741dec29f3c0c1c90c3e21a741e1ff65905c5
Offado <offolomebillchrisjonathanadon@gmail.com>
2024-07-11 23:13:22 +0000

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Card from 'react-bootstrap/Card';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <div className="App">
+                <Navbar bg="primary" data-bs-theme="dark">
+                    <Container>
+                    <Navbar.Brand href="#home">AGP</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Accueil</Nav.Link>
+                        <Nav.Link href="#features">Clients</Nav.Link>
+                        <Nav.Link href="#pricing">Rendez-vous</Nav.Link>
+                    </Nav>
+                    </Container>
+                </Navbar>
+
+                <br></br><br></br><br></br>
+
+                <h1 style={{ marginLeft: '20px' }}>Tableau de bord</h1>
+
+                <br></br>
+
+                <Card style={{ width: '18rem', float: 'left', display: 'flex', gap: '2rem', marginLeft: '30px' }}>
+                    <Card.Body>
+                        <Card.Title>Clients</Card.Title>
+                        <Card.Text>
+                        Nombre de clients: 20
+                        </Card.Text>
+                        <Card.Link href="#">Voir plus</Card.Link>
+                    </Card.Body>
+                </Card>
+
+                <Card style={{ width: '20rem', float: 'left', marginLeft: '50px' }}>
+                    <Card.Body>
+                        <Card.Title>Rendez-vous</Card.Title>
+                        <Card.Text>
+                        Vous avez rendez-vous le 07/07/2024
+                        </Card.Text>
+                        <Card.Link href="#">Voir plus</Card.Link>
+                    </Card.Body>
+                </Card>
+
+                <Card style={{ width: '20rem', float: 'left', marginLeft: '50px' }}>
+                    <Card.Body>
+                        <Card.Title>Statistiques</Card.Title>
+                        <Card.Text>
+                        Total clients: 80
+                        Total rendez-vous: 80
+                        </Card.Text>
+                        <Card.Link href="#">Voir plus</Card.Link>
+                    </Card.Body>
+                </Card>
+            </div>
+        </>
+    );
 }
 
 export default App;
